e66cf4a30da6ef8d5a31604b7a047f1f36474d4e
SerifOSalem <serif.salem@outlook.com>
2026-04-28 10:32:50 +0100

    Exercise 2 – Add Student Input Fields and Button

--- composeApp/src/jvmMain/kotlin/qho543/guistudentuniversityapp/App.kt
+++ composeApp/src/jvmMain/kotlin/qho543/guistudentuniversityapp/App.kt
@@ -1,21 +1,78 @@
 package qho543.guistudentuniversityapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
 @Composable
 fun App() {
 
-    // SnapshotStateList to store students
     val students = remember {
         mutableStateListOf<Student>()
     }
 
-    // For now, just an empty UI container
+    val idState = remember { mutableStateOf("") }
+    val nameState = remember { mutableStateOf("") }
+    val courseState = remember { mutableStateOf("") }
+    val markState = remember { mutableStateOf("") }
+
     MaterialTheme {
-        // UI will be added in next exercises
+        Column {
+            OutlinedTextField(
+                value = idState.value,
+                singleLine = true,
+                onValueChange = { idState.value = it },
+                label = { Text("Enter student ID") }
+            )
+
+            OutlinedTextField(
+                value = nameState.value,
+                singleLine = true,
+                onValueChange = { nameState.value = it },
+                label = { Text("Enter student name") }
+            )
+
+            OutlinedTextField(
+                value = courseState.value,
+                singleLine = true,
+                onValueChange = { courseState.value = it },
+                label = { Text("Enter course") }
+            )
+
+            OutlinedTextField(
+                value = markState.value,
+                singleLine = true,
+                onValueChange = { markState.value = it },
+                label = { Text("Enter mark") }
+            )
+
+            Button(
+                onClick = {
+                    val student = Undergraduate(
+                        idState.value,
+                        nameState.value,
+                        courseState.value
+                    )
+
+                    student.mark = markState.value.toIntOrNull() ?: 0
+
+                    students.add(student)
+
+                    idState.value = ""
+                    nameState.value = ""
+                    courseState.value = ""
+                    markState.value = ""
+                }
+            ) {
+                Text("Add Student")
+            }
+        }
     }
 }
 
